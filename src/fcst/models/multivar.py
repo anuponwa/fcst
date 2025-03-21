340c2d6ddf15549d348bd2ef9921fc698e144bc8
Anupong Wannakrairot <anuponwa@scg.com>
2025-03-21 17:05:08 +0700

    Add multivar models

--- src/fcst/models/multivar.py
+++ src/fcst/models/multivar.py
@@ -2,16 +2,26 @@ import pandas as pd
 from sktime.forecasting.base import ForecastingHorizon
 
 
-class MultivariateModel:
-    def __init__(self, model_cls_instance, val_col: str | None = None):
+class MultivariateModelWrapper:
+    def __init__(self, model_cls_instance, val_col: int | str | None = 0):
         """Generic wrapper for multi-variate models.
 
         Parameters
         ----------
             model_cls_instance (class instance): Multi-variate model instance
+
+            val_col (int | str | None): Value column of interest to return prediction as series (Default = 0)
+                If None, the result will return the whole dataframe.
+                If int is provided, it will return the predictions for that specific column index.
+                If string is provided, it will use that column name for the predictions.
+                In our convention, we use `y` in the first column, and other features are from [1:].
         """
 
         self.model = model_cls_instance
+        if val_col is not None:
+            if not isinstance(val_col, int) and not isinstance(val_col, str):
+                raise ValueError("`val_col` must be either int or str.")
+
         self.val_col = val_col
 
     def fit(self, y: pd.DataFrame, X=None, fh: ForecastingHorizon = None):
@@ -39,6 +49,11 @@ class MultivariateModel:
         self.df_pred = self.model.predict(fh=fh)
 
         if self.val_col is not None:
-            return self.df_pred[self.val_col]
+            if isinstance(self.val_col, int):
+                col = self.df_pred.columns[self.val_col]
+            if isinstance(self.val_col, str):
+                col = self.val_col
+
+            return self.df_pred[col]
 
         return self.df_pred
